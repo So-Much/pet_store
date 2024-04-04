fb3be235791d4af6c8b966d3f0b2816c8febf96f
duongdang2003 <duonghaidang01012003@gmail.com>
2024-04-04 22:05:53 +0700

    add scan function

--- backend/fastapi/main.py
+++ backend/fastapi/main.py
@@ -1,16 +1,60 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from io import BytesIO
+import onnxruntime
+import numpy as np
+from PIL import Image
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3000/scan",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class BaseItem(BaseModel):
-    name : str
-    description : str | None = None
-    price : float
-    tax : float
-    tags : list[str] = []
-    
+    prediction: list[list[float]]
 
 @app.get('/')
-async def root() -> str:
-    return "Hello World"
+async def root()    :
+    return "Server started"
+
+@app.post("/uploadImg")
+async def getImage(file : UploadFile = File(...)):
+    image = await file.read()
+    image = Image.open(BytesIO(image))
+    print(type(image))
+
+
+    image = image.resize((640, 640))  # Resize the image to match the desired shape
+
+    # Convert the image to a NumPy array
+    image_array = np.array(image)
+    # image_array = np.resize(image_array, (640, 640, 3))
+    image_array = image_array.astype(np.float32) / 255.0  # Normalize pixel values
+    image_array = np.transpose(image_array, (2, 0, 1))  # Transpose dimensions to match [3, 640, 640]
+    image_array = np.expand_dims(image_array, axis=0)  # Add batch dimension
+
+    # Load the ONNX model
+    onnx_model_path = "best.onnx"  # Replace "your_model.onnx" with the path to your ONNX model file
+    sess = onnxruntime.InferenceSession(onnx_model_path)
+
+    # Get the input name of the ONNX model
+    input_name = sess.get_inputs()[0].name
+
+    # Run inference
+    output = sess.run(None, {input_name: image_array})
+
+    # Assuming there is only one output, you can access it like this
+    output_tensor = output[0]
+    # print({"prediction": output_tensor.tolist()})
+    # return {"prediction": output_tensor.tolist()}
+    return JSONResponse(content=output_tensor.tolist())
--- frontend/src/pages/client/Scan.js
+++ frontend/src/pages/client/Scan.js
@@ -1,17 +1,303 @@
-import React from 'react'
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
+import React, { useEffect, useState } from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import { type } from "@testing-library/user-event/dist/type";
 
 export default function Scan() {
-  return (
-    <div className='mclient_header'>
-      <Header />
-      {/* main content */}
-      <div>
-        <h1>Scan</h1>
-        <p>Welcome to the Home page</p>
-      </div>
-      <Footer />
-    </div>
-  )
+	const [imgSource, setImgSource] = useState("");
+	const [uploaded, setUploaded] = useState(false);
+	const [file, setFile] = useState(null);
+	const [petName, setPetName] = useState("Your Pet Name");
+	const [percentage, setPercentage] = useState(0);
+	const petList = {
+		0: "Abyssinian",
+		1: "American Bulldog",
+		2: "American_pit_bull_terrier",
+		3: "Basset Hound",
+		4: "Beagle",
+		5: "Bengal",
+		6: "Birman",
+		7: "Bombay",
+		8: "Boxer",
+		9: "British Shorthair",
+		10: "Chihuahua",
+		11: "Egyptian Mau",
+		12: "English Cocker Spaniel",
+		13: "English Setter",
+		14: "German Shorthaired",
+		15: "Great pyrenees",
+		16: "Havanese",
+		17: "Japanese Chin",
+		18: "Keeshond",
+		19: "Leonberger",
+		20: "Maine_coon",
+		21: "Miniature_pinscher",
+		22: "Newfoundland",
+		23: "Persian",
+		24: "Pomeranian",
+		25: "Pug",
+		26: "Ragdoll",
+		27: "Russian Blue",
+		28: "Saint Bernard",
+		29: "Samoyed",
+		30: "Scottish Terrier",
+		31: "Shiba Inu",
+		32: "Siamese",
+		33: "Sphynx",
+		34: "Staffordshire Bull Terrier",
+		35: "Wheaten Terrier",
+		36: "Yorkshire Terrier",
+	};
+
+	const handleUpload = (file) => {
+		const formData = new FormData();
+		formData.append("file", file);
+		setPetName("");
+		fetch("http://127.0.0.1:8000/uploadImg", {
+			method: "POST",
+			body: formData,
+		})
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				console.log("Upload success ", data);
+				let predictionAr = data[0];
+				let max = 0;
+				let position = 0;
+				for (let i = 0; i < predictionAr.length; i++) {
+					if (max < predictionAr[i]) {
+						max = predictionAr[i];
+						position = i;
+					}
+				}
+				if (max > 0.8) {
+					setPetName(petList[position]);
+					setPercentage(Math.round(max * 10000) / 100);
+				}
+			})
+			.catch((error) => {
+				console.log("Error ", error);
+				setPetName("Cannot scan this image!");
+				setPercentage(0);
+			});
+	};
+	const uploadFile = (e) => {
+		if (e.target.files.length) {
+			const src = URL.createObjectURL(e.target.files[0]);
+			setImgSource(src);
+			setFile(e.target.files[0]);
+			setUploaded(true);
+		}
+	};
+
+	function LoadSection() {
+		if (!uploaded) {
+			return (
+				<div class="flex flex-1 items-center flex-col p-4">
+					<p class="relative inline cursor-pointer text-xl font-medium before:bg-violet-600  before:absolute before:-bottom-1 before:block before:h-[2px] before:w-full before:origin-bottom-right before:scale-x-0 before:transition before:duration-300 before:ease-in-out hover:before:origin-bottom-left hover:before:scale-x-100">
+						Take a photo
+					</p>
+					<h2 class="p-5">Or</h2>
+					<div class="border border-dashed border-gray-500 relative">
+						<input
+							type="file"
+							accept="image/*"
+							id="fileUploader"
+							class="cursor-pointer relative block opacity-0 w-full h-full p-20 z-50"
+							onChange={uploadFile}
+						/>
+						<div class="text-center p-10 absolute top-0 right-0 left-0 m-auto">
+							<h4>
+								Drop files anywhere to upload
+								<br />
+								or
+							</h4>
+							<p class="">Select Files</p>
+						</div>
+					</div>
+				</div>
+			);
+		} else {
+			return (
+				<div className="flex flex-1 items-center flex-col space-y-5">
+					<div className="flex">
+						<div class="m-3" style={{ height: "fit-content" }}>
+							<button
+								class="bg-white text-gray-800 font-bold rounded border-b-2 border-green-500 hover:border-green-600 hover:bg-green-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center"
+								id="scanBtn"
+								onClick={() => handleUpload(file)}
+							>
+								<span class="mr-2">Scan</span>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+								>
+									<path
+										fill="currentcolor"
+										d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"
+									></path>
+								</svg>
+							</button>
+						</div>
+
+						<div class="m-3">
+							<button
+								class="bg-white text-gray-800 font-bold rounded border-b-2 border-red-500 hover:border-red-600 hover:bg-red-500 hover:text-white shadow-md py-2 px-6 inline-flex items-center "
+								onClick={() => clearResult()}
+							>
+								<span class="mr-2">Close</span>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+								>
+									<path
+										fill="currentcolor"
+										d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"
+									/>
+								</svg>
+							</button>
+						</div>
+					</div>
+					<div className="flex flex-col">
+						<img className="" src={imgSource} id="img" />
+					</div>
+				</div>
+			);
+		}
+	}
+	function PetName() {
+		return (
+			<div class="flex flex-1 max-w-xs items-center border-l-8 border-emerald-500 bg-emerald-50 p-4 text-emerald-900 shadow-lg">
+				<div class="min-w-0">
+					<h2 class="overflow-hidden text-ellipsis whitespace-nowrap">
+						742 Evergreen Terrace, Springfield Country
+					</h2>
+				</div>
+			</div>
+		);
+	}
+	function clearResult() {
+		setUploaded(false);
+		setPetName("Your Pet Name");
+		setPercentage(0);
+	}
+	return (
+		<div className="mclient_header">
+			<Header />
+
+			{/* upload section */}
+			<div className="container flex justify-center items-center p-4">
+				<div class="flex w-4/5">
+					{LoadSection()}
+					<div className="flex flex-1 flex-col justify-center items-center p-4">
+						<div class="flex max-w-xs items-center border-l-8 border-emerald-500 bg-emerald-50 p-4 text-emerald-900">
+							<div class="min-w-0">
+								<h2 class="overflow-hidden text-ellipsis whitespace-nowrap text-xl">
+									{petName}
+								</h2>
+							</div>
+						</div>
+						<p className="p-2 text-xs">{"Accuracy: " + percentage + "%"}</p>
+						{/* table */}
+
+						<div class="flex min-h-screen justify-center">
+							<div class="overflow-x-auto">
+								<table class="min-w-full bg-white shadow-md rounded-xl">
+									<thead>
+										<tr class="bg-blue-gray-100 text-gray-700">
+											<th class="py-3 px-4 text-left">Stock Name</th>
+											<th class="py-3 px-4 text-left">Price</th>
+											<th class="py-3 px-4 text-left">Quantity</th>
+											<th class="py-3 px-4 text-left">Total</th>
+											<th class="py-3 px-4 text-left">Action</th>
+										</tr>
+									</thead>
+									<tbody class="text-blue-gray-900">
+										<tr class="border-b border-blue-gray-200">
+											<td class="py-3 px-4">Company A</td>
+											<td class="py-3 px-4">$50.25</td>
+											<td class="py-3 px-4">100</td>
+											<td class="py-3 px-4">$5025.00</td>
+											<td class="py-3 px-4">
+												<a
+													href="#"
+													class="font-medium text-blue-600 hover:text-blue-800"
+												>
+													Edit
+												</a>
+											</td>
+										</tr>
+										<tr class="border-b border-blue-gray-200">
+											<td class="py-3 px-4">Company B</td>
+											<td class="py-3 px-4">$75.60</td>
+											<td class="py-3 px-4">150</td>
+											<td class="py-3 px-4">$11340.00</td>
+											<td class="py-3 px-4">
+												<a
+													href="#"
+													class="font-medium text-blue-600 hover:text-blue-800"
+												>
+													Edit
+												</a>
+											</td>
+										</tr>
+										<tr class="border-b border-blue-gray-200">
+											<td class="py-3 px-4">Company C</td>
+											<td class="py-3 px-4">$30.80</td>
+											<td class="py-3 px-4">200</td>
+											<td class="py-3 px-4">$6160.00</td>
+											<td class="py-3 px-4">
+												<a
+													href="#"
+													class="font-medium text-blue-600 hover:text-blue-800"
+												>
+													Edit
+												</a>
+											</td>
+										</tr>
+										<tr class="border-b border-blue-gray-200">
+											<td class="py-3 px-4 font-medium">Total Wallet Value</td>
+											<td class="py-3 px-4"></td>
+											<td class="py-3 px-4"></td>
+											<td class="py-3 px-4 font-medium">$22525.00</td>
+											<td class="py-3 px-4"></td>
+										</tr>
+									</tbody>
+								</table>
+								<div class="w-full pt-5 px-4 mb-8 mx-auto ">
+									<div class="text-sm text-gray-700 py-1 text-center">
+										Made with{" "}
+										<a
+											class="text-gray-700 font-semibold"
+											href="https://www.material-tailwind.com/docs/html/table/?ref=tailwindcomponents"
+											target="_blank"
+										>
+											Material Tailwind
+										</a>{" "}
+										by{" "}
+										<a
+											href="https://www.creative-tim.com?ref=tailwindcomponents"
+											class="text-gray-700 font-semibold"
+											target="_blank"
+										>
+											{" "}
+											Creative Tim
+										</a>
+										.
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<Footer />
+		</div>
+	);
 }
